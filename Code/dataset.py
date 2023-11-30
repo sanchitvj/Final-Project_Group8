8e748d1cc4d9af738ee0aa54633ba784190abf93
sanchitvj <sanchitvj1026@gmail.com>
2023-11-29 21:33:32 -0500

    working on modeling

--- Code/dataset.py
+++ Code/dataset.py
@@ -21,8 +21,60 @@ print(f"transformers.__version__: {transformers.__version__}")
 
 class FeedbackDataset(Dataset):
 
-    def __init__(self):
-        pass
+    def __init__(self, cfg, df, tokenizer, test=False):
+        self.df = df.reset_index(drop=True)
+        self.classes = ['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']
+        self.max_len = cfg.dataset.max_len
+        self.tokenizer = tokenizer
+        self.test = test
 
-    def __getitem__(self, index) -> T_co:
-        self.items[index]
+    def __getitem__(self, index):
+        text = self.df["full_text"][index]
+        tokenization = self.tokenizer(text,
+                                      add_special_tokens=True,
+                                      max_length=self.max_len,
+                                      truncation=True,
+                                      return_offsets_mapping=False)
+
+        inputs = {
+            "input_ids": torch.tensor(tokenization['input_ids'], dtype=torch.long),
+            "token_type_ids": torch.tensor(tokenization['token_type_ids'], dtype=torch.long),
+            "attention_mask": torch.tensor(tokenization['attention_mask'], dtype=torch.long)
+        }
+
+        if self.test:
+            return inputs
+
+        label = self.df.loc[index, self.classes].to_list()
+        targets = {
+            "labels": torch.tensor(label, dtype=torch.float32),
+        }
+
+        return inputs, targets
+
+    def __len__(self) -> int:
+        return len(self.df)
+
+
+def collate_fn(batch):
+    # Separate input and target in the batch
+    inputs = [item[0] for item in batch]
+    targets = [item[1] for item in batch if len(item) > 1]
+
+    # Batch input
+    batched_input = {
+        'input_ids': torch.stack([x['input_ids'] for x in inputs]),
+        'token_type_ids': torch.stack([x['token_type_ids'] for x in inputs]),
+        'attention_mask': torch.stack([x['attention_mask'] for x in inputs])
+    }
+
+    # Check if targets are available
+    if len(targets) > 0:
+        batched_targets = {
+            'labels': torch.stack([x['labels'] for x in targets])
+        }
+        return batched_input, batched_targets
+
+    return batched_input
+
+
---
+++ Code/model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+from transformers import AutoModel, AutoConfig
+from torch.utils.checkpoint import checkpoint
+
+
+
+
+
